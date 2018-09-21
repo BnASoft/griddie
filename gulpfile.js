6053c020d5ed46411e63ea07c8a716d9c25fdabc
Daniele Fioroni - External <daniele.fioroni.external@ynap.com>
2018-09-21 17:31:34 +0200

    Easier build initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -23,32 +23,10 @@
 
 'use strict';
 
-const arg = (argList => {
-    let arg = {},
-        a,
-        opt,
-        thisOpt,
-        curOpt;
-    for (a = 0; a < argList.length; a++) {
-        thisOpt = argList[a].trim();
-        opt = thisOpt.replace(/^\-+/, '');
-
-        if (opt === thisOpt) {
-            if (curOpt) {
-                arg[curOpt] = opt;
-            }
-
-            curOpt = null;
-        } else {
-            curOpt = opt;
-
-            arg[curOpt] = true;
-        }
-    }
-
-    return arg;
-})(process.argv);
-
+const clearRequire = module => {
+    delete require.cache[require.resolve(module)];
+    return require(module);
+};
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 const gulpif = require('gulp-if');
@@ -65,162 +43,49 @@ const autoprefixer = require('autoprefixer');
 const log = require('fancy-log');
 const hb = require('gulp-hb');
 const clone = require('gulp-clone');
+const configuration = require('./gulpfile.json')[0];
+const resources = configuration.resources;
 
-const production = !arg.development;
-const clearRequire = module => {
-    delete require.cache[require.resolve(module)];
-    return require(module);
-};
-const buildConfig = require('./build.json')[0];
-const resources = buildConfig.resources;
-let processedFiles = {
+let processed = {
     css: [],
     js: [],
     html: []
 };
 
-gulp.task('build-js', callback => {
-    let pipe = [];
+// - - - - - - -
+// micro tasks
+// - - - - - - -
+// library:
+// cleanup
+gulp.task('library-clean', callback => {});
+// rollup js includes and transpile
+gulp.task('library-rollup', callback => {});
+// minify js
+gulp.task('library-min', callback => {});
+// test:
+// cleanup
+gulp.task('test-clean', callback => {});
+// transpile css
+gulp.task('test-css', callback => {});
+// rollup js includes and transpile
+gulp.task('test-rollup', callback => {});
+// build handlebar
+gulp.task('test-html', callback => {});
+// - - - - - - -
 
-    processedFiles.js = [];
-
-    resources.forEach(resource => {
-        if ('js' in resource.files && resource.files.js.list.length) {
-            const src = 'src' in resource.files.js ? resource.files.css.js : resource.paths.src;
-            const dst = 'dist' in resource.files.js ? resource.files.css.js : resource.paths.dist;
-
-            resource.files.js.list.forEach(filename => {
-                const source = src + filename;
-
-                processedFiles.js.push(source);
-
-                const cloneSink = clone.sink();
-
-                pipe = pipe.concat([
-                    // transpilation
-                    gulp.src(source),
-                    sourcemaps.init(),
-                    gulpif(
-                        'module' in resource.files.js && !!resource.files.js.module,
-                        rollup(
-                            {},
-                            {
-                                name: resource.files.js.module,
-                                format: 'umd'
-                            }
-                        ).on('error', err => log(err))
-                    ),
-                    babel().on('error', err => log(err)),
-                    cloneSink,
-                    gulp.dest(dst),
-                    // minification copy stream
-                    cloneSink.tap(),
-                    minify({ ext: { min: '.min.js' } }),
-                    sourcemaps.write('.'),
-                    gulp.dest(dst)
-                ]);
-            });
-        }
-    });
-
-    pump(pipe, callback);
+// - - - - - - -
+// main tasks
+// - - - - - - -
+// bundle library distribution
+gulp.task('bundle', ['test-clean', 'library-clean', 'library-rollup', 'library-min'], () => {
+    //gulp.watch(/* processed files ... */, [/* tasks ... */]);
 });
-
-gulp.task('build-css', callback => {
-    let pipe = [];
-
-    resources.forEach(resource => {
-        if ('css' in resource.files && resource.files.css.list.length) {
-            const src = 'src' in resource.files.css ? resource.files.css.src : resource.paths.src;
-            const dst = 'dist' in resource.files.css ? resource.files.css.src : resource.paths.dist;
-
-            resource.files.css.list.forEach(filename => {
-                const source = src + filename;
-
-                processedFiles.css.push(source);
-
-                const cloneSink = clone.sink();
-
-                pipe = pipe.concat([
-                    // transpilation
-                    gulp.src(source),
-                    sourcemaps.init(),
-                    sass({ outputStyle: 'nested', onError: err => log(err) }),
-                    postcss([autoprefixer()]).on('error', err => log(err)),
-                    cloneSink,
-                    cloneSink.tap(),
-                    sourcemaps.write('.'),
-                    gulp.dest(dst),
-                    // minification copy stream
-                    cloneSink.tap(),
-                    csso(),
-                    rename({ suffix: '.min' }),
-                    sourcemaps.write('.'),
-                    gulp.dest(dst)
-                ]);
-            });
-        }
-    });
-
-    pump(pipe, callback);
+// setup test page
+gulp.task('test', ['library-clean', 'test-clean', 'test-css', 'test-html'], () => {
+    //gulp.watch(/* processed files ... */, [/* tasks ... */]);
 });
-
-gulp.task('build-html', callback => {
-    let pipe = [];
-
-    resources.forEach(resource => {
-        if ('hbs' in resource.files && resource.files.hbs.list.length) {
-            const src = 'src' in resource.files.hbs ? resource.files.hbs.src : resource.paths.src;
-            const dst = 'dist' in resource.files.hbs ? resource.files.hbs.dist : resource.paths.dist;
-
-            resource.files.hbs.list.forEach(filename => {
-                const source = src + filename;
-
-                processedFiles.html.push(source);
-
-                const path = source.replace('.hbs', '');
-                const jsons = path + '.json';
-                const data = clearRequire(jsons);
-
-                processedFiles.html.push(path + '.json');
-
-                const hbs = path + '.*.hbs';
-                processedFiles.html.push(hbs);
-
-                pipe = pipe.concat([
-                    gulp.src(source),
-                    hb({ bustCache: true })
-                        .data(data)
-                        .data({
-                            library: {
-                                src: `../${production ? 'dist' : 'src'}/griddie${production ? '.min' : ''}.js`, // you'll need #enable-experimental-web-platform-features flag enabled under chrome://flags
-                                type: !production ? 'module' : 'text/javascript',
-                                attrs: ''
-                            },
-                            js: {
-                                src: `assets/${production ? 'dist' : 'src'}/test${production ? '.min' : ''}.js`,
-                                type: !production ? 'module' : 'text/javascript',
-                                attrs: !production ? 'defer' : ''
-                            },
-                            css: `assets/dist/test${production ? '.min' : ''}.css`,
-                            title: `Griddie.js ${production ? 'Playground' : 'Dev'}`
-                        })
-                        .partials(hbs),
-                    rename({ extname: '.html' }),
-                    gulp.dest(dst)
-                ]);
-            });
-        }
-    });
-
-    pump(pipe, callback);
+// setup test page (old browsers)
+gulp.task('test-es5', ['library-clean', 'test-clean', 'test-css', 'test-html'], () => {
+    //gulp.watch(/* processed files ... */, [/* tasks ... */]);
 });
-
-gulp.task('js', ['build-js'], () => gulp.watch(processedFiles.js, ['build-js']));
-gulp.task('css', ['build-css'], () => gulp.watch(processedFiles.css, ['build-css']));
-gulp.task('html', ['build-html'], () => gulp.watch(processedFiles.html, ['build-html']));
-gulp.task('default', ['build-js', 'build-css', 'build-html'], () => {
-    gulp.watch(processedFiles.js, ['build-js']);
-    gulp.watch(processedFiles.css, ['build-css']);
-    gulp.watch(processedFiles.html, ['build-html']);
-});
+// - - - - - - -
