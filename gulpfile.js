925f99b41ba9c5196b7a33dc77098e20e7600398
Daniele Fioroni - External <daniele.fioroni.external@ynap.com>
2018-09-24 11:42:44 +0200

    Build rework: clean dists

--- gulpfile.js
+++ gulpfile.js
@@ -29,53 +29,130 @@ const clearRequire = module => {
 };
 const gulp = require('gulp');
 const watch = require('gulp-watch');
-const gulpif = require('gulp-if');
 const pump = require('pump');
 const rename = require('gulp-rename');
 const rollup = require('gulp-better-rollup');
 const sourcemaps = require('gulp-sourcemaps');
-const csso = require('gulp-csso');
 const babel = require('gulp-babel');
+const del = require('del');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const minify = require('gulp-minify');
 const autoprefixer = require('autoprefixer');
 const log = require('fancy-log');
 const hb = require('gulp-hb');
-const clone = require('gulp-clone');
 const configuration = require('./gulpfile.json')[0];
-const resources = configuration.resources;
-
-let processed = {
-    css: [],
-    js: [],
-    html: []
-};
 
 // - - - - - - -
 // micro tasks
 // - - - - - - -
 // library:
 // cleanup
-gulp.task('library-clean', callback => {});
+gulp.task('library-clean', done => {
+    del.sync('dist');
+    done();
+});
 // rollup js includes and transpile
-gulp.task('library-rollup', callback => {});
+gulp.task('library-rollup', callback =>
+    pump(
+        [
+            /*gulp.src(),
+            sourcemaps.init(configuration.sourcemaps),
+            rollup({}, {...configuration.rollup, name : "Griddie"}).on('error', err => log(err)),
+            babel().on('error', err => log(err)),
+            sourcemaps.write('.'),
+            gulp.dest(resource.paths.dist)*/
+        ],
+        callback
+    )
+);
 // minify js
-gulp.task('library-min', callback => {});
+gulp.task('library-min', callback =>
+    pump(
+        [
+            /*gulp.src(resource.paths.src + filename),
+            sourcemaps.init(configuration.sourcemaps),
+            rollup({}, {...configuration.rollup, name : "Griddie"}).on('error', err => log(err)),
+            babel().on('error', err => log(err)),
+            minify({ ext: { min: '.min.js' } }),
+            sourcemaps.write('.'),
+            gulp.dest(resource.paths.dist)*/
+        ],
+        callback
+    )
+);
 // test:
 // cleanup
-gulp.task('test-clean', callback => {});
+gulp.task('test-clean', done => {
+    del.sync('test/assets/dist');
+    del.sync('test/index.html');
+    done();
+});
 // transpile css
-gulp.task('test-css', callback => {});
+gulp.task('test-css', callback => {
+    pump(
+        [
+            /*gulp.src(),
+            sourcemaps.init(configuration.sourcemaps),
+            sass({...configuration.sass, { onError: err => log(err) }}),
+            postcss([autoprefixer()]).on('error', err => log(err)),
+            sourcemaps.write('.'),
+            gulp.dest(resource.paths.dist)*/
+        ],
+        callback
+    );
+});
 // rollup js includes and transpile
-gulp.task('test-rollup', callback => {});
+gulp.task('test-rollup', callback =>
+    pump(
+        [
+            /*gulp.src(),
+            sourcemaps.init(configuration.sourcemaps),
+            rollup({}, {...configuration.rollup, name : "GriddieTest"}).on('error', err => log(err)),
+            babel().on('error', err => log(err)),
+            sourcemaps.write('.'),
+            gulp.dest(resource.paths.dist)*/
+        ],
+        callback
+    )
+);
 // build handlebar
-gulp.task('test-html', callback => {});
+gulp.task('test-html', callback =>
+    pump(
+        [
+            /*gulp.src(source),
+            hb(configuration.hbs)
+                .data(clearRequire('test/assets/src/index.json'))
+                .data()
+                .partials(),
+            rename({ extname: '.html' }),
+            gulp.dest(dst)*/
+        ],
+        callback
+    )
+);
+// build handlebar es5
+gulp.task('test-html', callback =>
+    pump(
+        [
+            /*gulp.src(source),
+            hb(configuration.hbs)
+                .data(clearRequire('test/assets/src/index.json'))
+                .data()
+                .partials(),
+            rename({ extname: '.html' }),
+            gulp.dest(dst)*/
+        ],
+        callback
+    )
+);
 // - - - - - - -
 
 // - - - - - - -
 // main tasks
 // - - - - - - -
+// clean all
+gulp.task('clean', ['library-clean', 'test-clean']);
 // bundle library distribution
 gulp.task('bundle', ['test-clean', 'library-clean', 'library-rollup', 'library-min'], () => {
     //gulp.watch(/* processed files ... */, [/* tasks ... */]);
@@ -85,7 +162,7 @@ gulp.task('test', ['library-clean', 'test-clean', 'test-css', 'test-html'], () =
     //gulp.watch(/* processed files ... */, [/* tasks ... */]);
 });
 // setup test page (old browsers)
-gulp.task('test-es5', ['library-clean', 'test-clean', 'test-css', 'test-html'], () => {
+gulp.task('test-es5', ['library-clean', 'test-clean', 'test-css', 'test-rollup', 'test-html-es5'], () => {
     //gulp.watch(/* processed files ... */, [/* tasks ... */]);
 });
 // - - - - - - -
